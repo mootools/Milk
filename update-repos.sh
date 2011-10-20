5dd9f9108f2359b0c4ad9ff795cde14806fd48df
Olmo Maldonado <olmo.maldonado@gmail.com>
2011-10-19 20:29:13 -0500

    Feeble attempt to update the repos with a single bash script. Updating subprojects as well.
    small change to remove warning from find in linux.
    
    added check for git repo. removing stderr for git checkout and cd echos
    
    Optimizing the checkout to remote/master
    
    Leave it to arian to come up with an even better script :D

---
+++ update-repos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+for repo in `find . -maxdepth 2 -mindepth 2 -wholename '*/.git'`
+do
+	cd $repo/..
+	echo "updating $(pwd)"
+	remote='origin'
+	if [[ `git remote | grep mootools` ]]; then
+		remote='mootools'
+	fi
+	git fetch $remote
+	git reset --hard $remote/master
+	cd ..
+done
